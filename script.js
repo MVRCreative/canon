96bcfc0ad1fd62d5ebfb9b3b2515c3456143da1b
MVRCreative <154540859+MVRCreative@users.noreply.github.com>
2024-11-06 15:01:32 -0500

    added swiper

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Hello World");
+.swiper-slide {
+  height: auto !important;
+}
+
+const swiper = new Swiper('.swiper', {
+  effect: 'coverflow',
+  coverflowEffect: {
+    rotate: 10,
+    slideShadows: false,
+    stretch: 32,
+ 
+  },
+  	loop: true,
+    slidesPerView: 2,
+    slideActiveClass: "is--active",
+    centeredSlides: true, 
+    dragClass: 'swiper-scrollbar-drag',
+    draggable: true,
+    grabCursor: true,
+    spaceBetween: 30,
+    
+    
+});
+</script>
+
+
+.swiper-slide {
+    height: auto !important;
+
